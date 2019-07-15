004619e53f8c7eb6cc66e0430518954d90bfaff6
cjw363 <asd6555363>
2019-07-15 15:25:34 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -43,13 +43,13 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './', //打包前改成。/    之前/
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,  //打包前改成false
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
